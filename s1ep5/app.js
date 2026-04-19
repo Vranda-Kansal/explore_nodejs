e22b6a51581b1fa2af6fd1ba180c49fcf00ec793
Vranda-Kansal <itzvaniaggarwal12@gmail.com>
2026-04-19 14:13:48 +0530

    send a response

--- s1ep5/app.js
+++ s1ep5/app.js
@@ -1,5 +1,7 @@
 const http = require("http");
 
-const server = http.createServer();
+const server = http.createServer(function (req, res) {
+  res.end("hello world");
+});
 
 server.listen(3000, () => console.log("running"));
